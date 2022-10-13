b27d7c037eca89ef6b4d9dd6700fa2341b8a6f5b
MarcelaLopez <marc3lyn@gmail.com>
2022-10-12 22:13:50 -0600

    Setting up the cypress mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,27 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
+
   projectId: "8fcew8",
   chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'testing-cypress-report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: 'cypress/report'
+  },
+  
   
   e2e: {
+    //This active the Cypress Studio
+    experimentalStudio: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      require('cypress-mochawesome-reporter/plugin')(on);      
     },
   },
 });
